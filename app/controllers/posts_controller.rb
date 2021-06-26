f3c44584aed291bf586c2d8a8c291c6c5d0a542f
Akustino0310 <niggy2014@yahoo.co.jp>
2021-06-26 18:21:49 +0900

    新規投稿フォームを実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+  PER_PAGE = 10
+ 
   def index
-    @posts = Post.all.order(id: "DESC")
+    @posts = Post.order(id: :desc).page(params[:page]).per(PER_PAGE)
   end
 
   def create
